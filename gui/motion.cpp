708cd59f7871ac3c708bb3b2e24c340ed565b002
Dániel Arató <nil.the.human@gmail.com>
2015-05-09 18:02:22 +0200

    Add Motion class and subclasses

---
+++ gui/motion.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015 Dániel Arató
+ *
+ * This file is part of Retra.
+ *
+ * Retra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Retra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Retra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Different types of Object motions
+// Part of Retra, the Reference Tracer
+
+#include "motion.h"
+
+#include <cmath>
+#include <cstdlib>
+
+namespace Retra {
+
+    void BrownianMotion::step( double dt ) const
+    {
+        double x = std::rand() * 2;
+        if ( RAND_MAX < x )
+            x = RAND_MAX - x;
+        double y = std::rand() * 2;
+        if ( RAND_MAX < y )
+            y = RAND_MAX - y;
+        double z = std::rand() * 2;
+        if ( RAND_MAX < z )
+            z = RAND_MAX - z;
+        Vector delta = Vector( x, y, z ).normalized() * scale;
+
+        object->move( delta * dt );
+    }
+
+    void LinearMotion::step( double dt ) const
+    {
+        object->move( delta * dt );
+    }
+
+    void OrbitingMotion::step( double dt ) const
+    {
+        object->move( 2.0 * PI * dt / period, axis );
+    }
+
+    void OscillatingMotion::step( double dt ) const
+    {
+        object->move( (end - begin) / PI * sin(phase) * dt );
+        phase += 2.0 * PI * dt / period; // TODO: don't let phase overflow.
+        std::cout << "move: " << (end - begin) * sin(phase) * dt << std::endl;
+    }
+
+}
+
---
+++ gui/motion.h
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2015 Dániel Arató
+ *
+ * This file is part of Retra.
+ *
+ * Retra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Retra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Retra.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Motion classes to describe how a given Object is supposed to move around the Scene
+// Part of Retra, the Reference Tracer
+
+#ifndef RETRA_MOTION
+#define RETRA_MOTION
+
+#include "../core/scene.h"
+#include "../core/triplet.h"
+
+namespace Retra {
+
+    class Object;
+
+    class Motion {
+    public:
+        Motion( const Object* object ) : object( object ) { }
+        virtual ~Motion() { }
+
+        virtual void step( double dt ) const = 0; // Apply the displacement between the current and the next "frame" to the Object
+
+    protected:
+        const Object* const object;
+    };
+
+    // Random motion each time
+    class BrownianMotion : public Motion {
+    public:
+        BrownianMotion( const Object* object, double scale = 1.0 )
+            : Motion( object )
+            , scale( scale )
+        { }
+
+        virtual void step( double dt ) const;
+
+    private:
+        double scale;
+    };
+
+    // Move in a constant direction at constant speed
+    class LinearMotion : public Motion {
+    public:
+        LinearMotion( const Object* object, const Vector& delta )
+            : Motion( object )
+            , delta( delta )
+        { }
+
+        virtual void step( double dt ) const;
+
+    private:
+        Vector delta;
+    };
+
+    // Rotate around fixed world axis
+    class OrbitingMotion : public Motion {
+    public:
+        OrbitingMotion( const Object* object, WorldAxis axis, double period )
+            : Motion( object )
+            , axis( axis )
+            , period( period )
+        { }
+
+        virtual void step( double dt ) const;
+
+    private:
+        WorldAxis axis;
+        double    period;
+    };
+
+    // Cosine wave between two points
+    class OscillatingMotion : public Motion {
+    public:
+        OscillatingMotion( const Object* object, const Vector& begin, const Vector& end, double period )
+            : Motion( object )
+            , begin( begin )
+            , end( end )
+            , period( period )
+            , phase( 0 )
+        { }
+
+        virtual void step( double dt ) const;
+
+    private:
+        Vector begin;
+        Vector end;
+        double period;
+        mutable double phase; // Current state
+    };
+
+}
+
+#endif // RETRA_MOTION
+
