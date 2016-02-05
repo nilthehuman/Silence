ef943ed6d43a5b2e1a9f00bc3543e74ff52bc2cd
Dániel Arató <nil.the.human@gmail.com>
2016-02-05 01:43:08 +0100

    Add Shadow class

---
+++ src/core/shadow.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Dániel Arató
+ *
+ * This file is part of Silence.
+ *
+ * Silence is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Silence is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Silence.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Shadows are used to track the unlit subvolumes of a Zone
+// Part of Silence, an experimental rendering engine
+
+#ifndef SILENCE_SHADOW
+#define SILENCE_SHADOW
+
+#include "beam.h"
+
+namespace Silence {
+
+    class Shadow {
+    public:
+        Shadow( const Beam& umbra, const Beam& penumbra )
+            : umbra( umbra )
+            , penumbra( penumbra )
+        { }
+
+        double occluded( const Vector& point ) const;
+
+    private:
+        Beam umbra;    // Part completely occluded from the lightsource
+        Beam penumbra; // Part partially  occluded from the lightsource
+    };
+
+#endif // SILENCE_SHADOW
+
